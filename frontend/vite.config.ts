db930436eb647b40db8dc61d9d46b7fb75f15be7
BARSOT Thomas <thomas.barsot@insa-rouen.fr>
2026-04-09 01:01:47 +0200

    clear

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,20 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // ⚠️ Très important pour Vercel
+  base: '/',   // essentiel pour que tous les assets soient relatifs à la racine
   plugins: [react()],
-  server: {
-    proxy: {
-      '/callback': 'http://127.0.0.1:8080',
-      '/login': 'http://127.0.0.1:8080',
-      '/api': 'http://127.0.0.1:8080'
-    },
-    host: 'localhost',
-    port: 5173,
-    hmr: {
-      host: 'localhost',
-      protocol: 'ws',
-      port: 5173
-    }
-  }
 })
